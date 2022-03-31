1119b60860e29e36ec3d3237b3b161b43fce49b0
Lukas Kluft <lukas.kluft@gmail.com>
2022-04-01 00:30:41 +0200

    Use CSS properties to set plot style

--- plot.js
+++ plot.js
@@ -1,12 +1,16 @@
 import { getDataVar } from "./zarr_data.js";
 
-
+let style = window.getComputedStyle(document.getElementById('spectrum'));
 var layout = {
-    font: {size: 18, family: "Roboto Slab"},
-    paper_bgcolor: "#0E0E10",
-    plot_bgcolor: "#0E0E10",
-    xaxis: {color: "#FFFFFF"},
-    yaxis: {color: "#FFFFFF"},
+    font: {
+        family: style.getPropertyValue('font-family'),
+        color: style.getPropertyValue('color'),
+        size: style.getPropertyValue('font-size').slice(0, -2),
+    },
+    paper_bgcolor: style.getPropertyValue('background-color'),
+    plot_bgcolor: style.getPropertyValue('background-color'),
+    xaxis: {title: "Frequency / Hz"},
+    yaxis: {title: "OLR / Wm<sup>−2</sup>cm"},
 };
 
 var trace = {
